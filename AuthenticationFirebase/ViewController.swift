7a512d11daa5e96155abd2ffed4d798d136ecd59
Mindy Douglas <mindy.douglas@icloud.com>
2023-02-24 09:48:00 -0600

    Initial Commit

---
+++ AuthenticationFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthenticationFirebase
+//
+//  Created by Mindy Douglas on 2/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
